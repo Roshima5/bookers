af5828a1e30cb32843d4329dd156b40d770f1bf6
RisaOshima <107258614+Roshima5@users.noreply.github.com>
2022-08-05 15:21:02 +0000

    [add]ファイルの記述・CRUDの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,14 @@
 class BooksController < ApplicationController
   def new
-    
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+    else
+      render :new 
+    end
   end
 
   def index
@@ -21,7 +23,19 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
-
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
+  def destroy
+    book = Book.find(params[:id])  # データ（レコード）を1件取得
+    book.destroy  # データ（レコード）を削除
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+  end
+  
   private
   def book_params
     params.require(:book).permit(:title, :body)
