ad89c34448357ba14dd96959e3649322231e5b8d
RisaOshima <107258614+Roshima5@users.noreply.github.com>
2022-08-06 11:25:33 +0000

    [finish]バリデーションの実装まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,9 +5,11 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to book_path(@book.id), notice: 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+      flash[:notice] = 'Book was successfully created.'
     else
-      render :new 
+      @books = Book.all
+      render :index
     end
   end
 
@@ -25,15 +27,20 @@ class BooksController < ApplicationController
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = 'Book was successfully updated.'
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
     book = Book.find(params[:id])  # データ（レコード）を1件取得
     book.destroy  # データ（レコード）を削除
-    redirect_to '/books'  # 投稿一覧画面へリダイレクト  
+    redirect_to '/books'  # 投稿一覧画面へリダイレクト 
+    flash[:notice] = 'Book was successfully destroyed.'
   end
   
   private
